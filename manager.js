6d03685b39c0d9596c4c8d10ac7889e9f34420f5
Orikingium Studios <25711694+AbnerSquared@users.noreply.github.com>
2019-03-11 19:04:06 -0500

    Added more information; modified several existing CSS classes to polish up the format

--- manager.js
+++ manager.js
@@ -323,19 +323,6 @@ var modules =
 
 window.onload = function()
 {
-    var _navBar = document.getElementById("nav-bar");
-    document.getElementById("nav-toggle").onclick = function()
-    {
-        if (_navBar.style.display == 'inline-block')
-        {
-            _navBar.style.display = 'none';
-        }
-        else
-        {
-            _navBar.style.display = 'inline-block';
-        }
-    };
-    
     modules.forEach(function(_module)
     {
         // add the module info to the main info box.
